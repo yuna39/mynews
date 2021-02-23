e38740ce8c3334ddd7fc2ff2a781d3596f62582f
粟野 <yuuuuuna39@gmail.com>
2021-02-23 04:16:52 +0000

    課題１１

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>課題４画面</h1>
-    </body>
-</html>
+@section('title','Myプロフィール')
+
+
+@section('content')
+   <div class="container">
+       <div class="row">
+           <div class="col-md-8 mx-auto">
+               <h2>My プロフィール</h2>
+           </div>
+       </div>
+   </div>
+@endsection
