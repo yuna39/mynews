274bd0f8056613935f4393603f01fae66a9aa4b6
粟野 <yuuuuuna39@gmail.com>
2021-02-25 13:39:57 +0000

    課題１２

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,17 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 
 // 課題３
     Route::get('XXX', 'Admin\AAAController@bbb');
     
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
 
 
