04fb2abe611dec142f473065ec6fa5a905802f0a
粟野 <yuuuuuna39@gmail.com>
2021-02-26 10:20:55 +0000

    PHP/laravel10の再提出

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.profile')
+
+@section('title','プロフィールの編集')
+
+
+@section('content')
+   <div class="container">
+       <div class="row">
+           <div class="col-md-8 mx-auto">
+               <h2>プロフィールの編集</h2>
+           </div>
+       </div>
+   </div>
+@endsection
