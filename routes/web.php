d7b534bc9a008a93e4b600785d9446c98b623616
粟野 <yuuuuuna39@gmail.com>
2021-02-28 10:31:50 +0000

    PHP/Laravel13 課題提出

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
 
 // 課題３
     Route::get('XXX', 'Admin\AAAController@bbb');
     
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 
 Auth::routes();
